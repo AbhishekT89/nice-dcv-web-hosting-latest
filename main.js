b5c0f650b627e18c73cf3da6fd8bb12a1b7e91c6
Abhishek Tiwari <86018531+AbhishekT89@users.noreply.github.com>
2024-01-10 14:27:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ console.log("Using NICE DCV Web Client SDK version " + dcv.version.versionStr);
 //     main();
 // }
 
-function main() {
+export const main = () => {
   console.log("Setting log level to INFO");
   dcv.setLogLevel(dcv.LogLevel.INFO);
   r = document.getElementById("inputBar");
